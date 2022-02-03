c0300bd31fea62465cc0662acf1f85d0727266b8
Diego Costa <diegomcosta@outlook.com.br>
2022-02-03 16:12:24 -0300

    Modificação Exercicios 44
    Exercício 44 modificado.

--- Mundo 2 - Estruturas de Controle/ex044.py
+++ Mundo 2 - Estruturas de Controle/ex044.py
@@ -11,3 +11,17 @@ print("[ 4 ] 3x ou mais no cartão\n")
 
 op = int(input("Qual é a opção? "))
 
+if (op == 1):
+    npreco = preco - (preco * 0.1)  
+elif (op == 2):
+    npreco = preco - (preco * 0.05)
+elif (op == 3):
+    parcela = preco / 2
+    print(f"Sua compra será parcelada em 2X de R${parcela:.2F} SEM JUROS")
+elif (op == 4):
+    parcela = int(input("Quantas parcelas? "))
+    npreco = preco + (preco * 0.2)
+    valparcela = npreco / parcela
+    print(f"Sua compra será parcelada em {parcela}X de R${valparcela:.2f} COM JUROS",end=" ")
+    print(f"de valor R${preco * 0.2:.2f}")
+print(f"Sua compra de R${preco:.2f} vai custar R${npreco:.2f} no final.")
